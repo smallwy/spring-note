df9e85f8dc30ea37a6c4f2c25431f401133dcc6c
stone <742868088@qq.com>
2019-10-23 10:17:52 +0800

    测试启动加入beanPostProcesser

--- test/src/main/java/stone/spring/SourceCode1/Test.java
+++ test/src/main/java/stone/spring/SourceCode1/Test.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Test {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext(AppConfig.class);
+		AnnotationConfigApplicationContext applicationContext=new AnnotationConfigApplicationContext();
+		applicationContext.register(AppConfig.class);
+		applicationContext.addBeanFactoryPostProcessor(new MyBeanPostProcesser());
+		applicationContext.refresh();
 		UserDao userDao= (UserDao) applicationContext.getBean("userDao");
 		userDao.print();
 	}
